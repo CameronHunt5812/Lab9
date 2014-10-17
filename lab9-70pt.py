566b654bd3cd202fafeccded9eea2f584d0b6392
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-10-17 15:06:15 -0500

    Cel to fahrenheit converter

--- lab9-70pt.py
+++ lab9-70pt.py
@@ -8,7 +8,12 @@
 # Multiply by 9, then divide by 5, then add 32 to calculate your answer.
 
 # TODO:
+
 # Ask user for Celcius temperature to convert
+print "what tempurature do you want to convert"
 # Accept user input 
+num1 = int(raw_input())
 # Calculate fahrenheit
+ancer = ((num1 * 9) / 5) + 32
 # Output answer
+print "the equivilent fahrenheit temprature is" + str(ancer)
