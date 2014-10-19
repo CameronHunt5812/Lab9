1961efb9096712ac46fb57df8a7ba6ea8d8021f8
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-10-19 16:41:08 -0500

    lab9 85 pt
    surches thrue a list and makes a new list of only the numbers over 100

--- lab9-85pt.py
+++ lab9-85pt.py
@@ -8,9 +8,12 @@
 # and create a new list that includes only numbers greater than 100
 
 myList = [102,98,96,101,100,99,103,97,98,105]
-
+feverList = []
 # Insert for loop here
-
+for i in myList:
+    if i > 100:
+        feverList.append(i)
 
 # This should print [102,101,103,105]
 print myList
+print feverList
