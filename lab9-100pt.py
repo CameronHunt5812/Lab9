ab9267815757500af9537dd51073ee5e63edc4a1
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-10-21 15:09:39 -0500

    shuld go to hospis?

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -6,7 +6,13 @@
 
 # Create a program that tests if patients needs to be admitted to the hospital.
 # Ask the user for their temperature, and if they have been sick in the last 24 hours.
+print "what is your temperature?"
+temp = int(raw_input())
+print "have you been sick in the last 24 hours?"
+print "1 = yes 0 = no"
+sick = int(raw_input())
 # Additionally, ask if the user has recently travelled to West Africa.
+print "have you rescently travled to west africa?"
 # The program should continue to run until there are no patients left to diagnose (i.e. a while loop).
 
 # Criteria for Diagnosis:
