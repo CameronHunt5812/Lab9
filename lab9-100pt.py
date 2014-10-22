c33f1fe9788705d53b3b862e5c038ccc91d3e7d9
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-10-22 12:11:10 -0500

    lab 9 100 point

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -6,31 +6,27 @@
 
 # Create a program that tests if patients needs to be admitted to the hospital.
 # Ask the user for their temperature, and if they have been sick in the last 24 hours.
-
-
-
 # Additionally, ask if the user has recently travelled to West Africa.
-
 # The program should continue to run until there are no patients left to diagnose (i.e. a while loop).
 
 # This tells the person they shuld go to the hospital
 def ShuldGo():
     print "You shuld go tho the hospital"
     #asking how many more patients
-    print "any more patients?"
+    print "Any more patients?"
     print "1 = yes, 0 = no"
     
 patients = 1
 while (patients == 1):
     #asking for temp
-    print "what is your temperature?"
+    print "What is your temperature?"
     temp = int(raw_input())
     #asking if sick in the last 24 hours
-    print "have you been sick in the last 24 hours?"
+    print "Have you been sick in the last 24 hours?"
     print "1 = yes, 0 = no"
     sick = int(raw_input())
     #asking if bean to west africa recently
-    print "have you rescently travled to west africa?"
+    print "Have you rescently travled to west africa?"
     print "1 = yes, 0 = no"
     westAfrica = int(raw_input())
 # - A temperature of over 105F
@@ -45,3 +41,10 @@ while (patients == 1):
     elif temp > 100 or sick == 1 and westAfrica == 1:
         ShuldGo()
         patients = int(raw_input())
+    else:
+        print "you shuld stay home"
+        print "Any more patients?"
+        print "1 = yes, 0 = no"
+        patients = int(raw_input())
+
+print "Good by"
