7c55b6f1d2c92978466f7215489982ade687cc1c
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-10-22 11:54:52 -0500

    lab9 100 pont v4
    reworking the funktion to only print a question

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -12,15 +12,16 @@
 # Additionally, ask if the user has recently travelled to West Africa.
 
 # The program should continue to run until there are no patients left to diagnose (i.e. a while loop).
+
+patients = 1
+
+# This tells the person they shuld go to the hospital
 def ShuldGo():
-    print "You shuld go tho the "
+    print "You shuld go tho the hospital"
     #asking how many more patients
     print "any more patients?"
     print "1 = yes, 0 = no"
-    patients = int(raw_input())
-    
 
-patients = 1
 while (patients == 1):
     #asking for temp
     print "what is your temperature?"
@@ -32,18 +33,14 @@ while (patients == 1):
     #asking if bean to west africa recently
     print "have you rescently travled to west africa?"
     print "1 = yes, 0 = no"
-    westAfrica = int(raw_input())    
-    if temp > 105:
+    westAfrica = int(raw_input())
 # - A temperature of over 105F
-        print "you shuld go to the hospital"
-        patients = patients - 1
-        #asking how many more patients
-        askForPat()
+    if temp > 105:
+        ShuldGo()
+        patients = int(raw_input())
 # - A temperature of over 102F and they have been sick in the last 24 hours
     elif temp > 102 and sick == 1:
-        print "you Shuld go to the hospital"
-        patients = patients - 1
-        #asking how many more patients
-        askForPat()
+        
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
     elif temp > 100 or sick == 1 and westAfrica == 1:
+        
