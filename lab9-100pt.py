f92b5ad568a1025fd9d83dd6db2b554e8da008b0
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-10-22 11:43:50 -0500

    lab 9 100 pont
    refining and making funktions to call

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -12,12 +12,16 @@
 # Additionally, ask if the user has recently travelled to West Africa.
 
 # The program should continue to run until there are no patients left to diagnose (i.e. a while loop).
-patients = 1
-while (patients == 1):
+def ShuldGo():
+    print "You shuld go tho the "
     #asking how many more patients
     print "any more patients?"
     print "1 = yes, 0 = no"
     patients = int(raw_input())
+    
+
+patients = 1
+while (patients == 1):
     #asking for temp
     print "what is your temperature?"
     temp = int(raw_input())
@@ -33,16 +37,13 @@ while (patients == 1):
 # - A temperature of over 105F
         print "you shuld go to the hospital"
         patients = patients - 1
+        #asking how many more patients
+        askForPat()
 # - A temperature of over 102F and they have been sick in the last 24 hours
     elif temp > 102 and sick == 1:
         print "you Shuld go to the hospital"
         patients = patients - 1
+        #asking how many more patients
+        askForPat()
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
     elif temp > 100 or sick == 1 and westAfrica == 1:
-        print "you need to go to the hospital"
-        patients = patients - 1
-        
-# Criteria for Diagnosis:
-
-
-
